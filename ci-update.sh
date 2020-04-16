70376b7e5f9e6db963c555167ec03eebca1712e6
Pietro Albini <pietro@pietroalbini.org>
2020-04-16 18:27:39 +0200

    try to use GHA's own token to push updates

--- ci-update.sh
+++ ci-update.sh
@@ -36,8 +36,13 @@ if [[ -z "${GITHUB_ACTIONS+x}" ]]; then
 fi
 
 
-if [[ -z "${HIGHFIVE_GH_TOKEN}" ]]; then
+if [[ -z "${HIGHFIVE_GH_TOKEN+x}" ]]; then
     echo "Error: the \$HIGHFIVE_GH_TOKEN environment variable is not set!"
+    exit 1
+fi
+
+if [[ -z "${GITHUB_TOKEN+x}" ]]; then
+    echo "Error: the \$GITHUB_TOKEN environment variable is not set!"
     exit 1
 fi
 
@@ -54,5 +59,5 @@ else
         -c "user.name=${GIT_NAME}" \
         -c "user.email=${GIT_EMAIL}" \
         commit -m "${GIT_COMMIT_MESSAGE}"
-    git push "https://x-token:${HIGHFIVE_GH_TOKEN}@github.com/${GIT_REPO}" "${GIT_BRANCH}"
+    git push "https://x-token:${GITHUB_TOKEN}@github.com/${GIT_REPO}" "${GIT_BRANCH}"
 fi
