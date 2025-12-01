0a515048fc4b5f1095ba991070d27a11d1744e51
xd009642 <danielmckenna93@gmail.com>
2025-12-01 00:15:17 +0000

    All the data now saved into CSV.
    Need to go over the github one some more and make sure I haven't missed
    any required functionality then integrate properly.

--- src/gitlab/mod.rs
+++ src/gitlab/mod.rs
@@ -26,6 +26,13 @@ query ListRustRepos($after: String) {
       fullPath
       path
       webUrl
+      repository {
+        cargoFiles: blobs(paths: ["Cargo.toml", "Cargo.lock"] ref: "HEAD") {
+          nodes {
+            path
+          }
+        }
+      }
     }
   }
 }
@@ -46,6 +53,49 @@ struct Project {
     full_path: String,
     path: String,
     web_url: String,
+    repository: Option<Repository>,
+}
+
+impl Project {
+    fn has_cargo_toml(&self) -> bool {
+        match &self.repository {
+            Some(repo) => repo
+                .cargo_files
+                .nodes
+                .iter()
+                .any(|x| x.path == "Cargo.toml"),
+            None => false,
+        }
+    }
+
+    fn has_cargo_lock(&self) -> bool {
+        match &self.repository {
+            Some(repo) => repo
+                .cargo_files
+                .nodes
+                .iter()
+                .any(|x| x.path == "Cargo.lock"),
+            None => false,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Repository {
+    cargo_files: FilesNode,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct FilesNode {
+    nodes: Vec<FilePath>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct FilePath {
+    path: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -61,7 +111,7 @@ struct ApiData {
 }
 
 #[derive(Debug, Deserialize)]
-struct GraphQLResponse {
+struct GraphQlResponse {
     data: Option<ApiData>,
     errors: Option<serde_json::Value>,
 }
@@ -73,18 +123,16 @@ pub fn scrape(data: &Data, config: &Config, should_stop: &AtomicBool) -> Fallibl
     let mut page = 1;
 
     while !should_stop.load(Ordering::SeqCst) {
-        println!("Fetching page {page}...");
-
         let variables = serde_json::json!({ "after": after });
 
-        let resp: GraphQLResponse = client
+        let resp: GraphQlResponse = client
             .post(GITLAB_GRAPHQL_ENDPOINT)
             .json(&serde_json::json!({
                 "query": GRAPHQL_QUERY_REPOSITORIES,
                 "variables": variables
             }))
             .send()?
-            .json()?;
+            .text()?;
 
         if let Some(errors) = resp.errors {
             eprintln!("GraphQL errors: {errors:#?}");
@@ -92,24 +140,21 @@ pub fn scrape(data: &Data, config: &Config, should_stop: &AtomicBool) -> Fallibl
         }
 
         let gitlab_data = resp.data.expect("No data returned");
-        println!("{:?}", gitlab_data);
         let projects = gitlab_data.projects;
 
         for project in projects.nodes {
-            println!("{:?}", project);
             data.store_repo(
                 "gitlab",
                 Repo {
                     id: project.id.clone(),
                     name: project.full_path.to_string(),
-                    has_cargo_toml: true, // TODO set
-                    has_cargo_lock: true,
+                    has_cargo_toml: project.has_cargo_toml(),
+                    has_cargo_lock: project.has_cargo_lock(),
                 },
             )?;
         }
 
         if !projects.page_info.has_next_page {
-            println!("No more pages");
             break;
         }
 
